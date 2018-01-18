773cfc413715e7777897a3cee7698ba729dbf9f2
7grant2 <grantjr7@gmail.com>
2018-01-17 19:46:09 -0500

    style updates

--- src/build/monitor.php
+++ src/build/monitor.php
@@ -1,27 +1,30 @@
 <?php
-    //file:///Users/glanham/Documents/CurrentProjects/BuildingManagementSystem/build/index.html
-?>
-<?php
-  include_once 'header.php';
-?>
-
+include_once 'header.php';
+if (!isset($_SESSION['u_id'])) {
+    header("Location: index.php");
+    exit();
+} else {
+    echo "
     <div class='jumbotron'>
-      <div class='row text-center'>
-	  <h1>Monitor</h1>
-	  </div>
+    <div class='row text-center'>
+    <h1>Monitor</h1>
+    </div>
     </div>
 
-    <div class='jumbotron'>
-      <div class='row text-center'>
-      </div>
-    </div>
-
-
-
-
-
+    <div class='jumbotron col-sm-10 col-sm-offset-1'>
     
-<?php
-    include_once 'footer.php';
+";
+    include_once 'includes/monitor.inc.php';
+    echo "
+    </div>
+
+";
+}
+
+
+include_once 'footer.php';
 ?>
-      
+
+
+
+
