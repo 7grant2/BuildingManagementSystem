ffaac1f6046d26b945887d4ff5640efaba19fea3
7grant2 <grantjr7@gmail.com>
2018-02-01 20:52:03 -0500

    added basic api functionality

---
+++ src/build/api/index.php
@@ -0,0 +1,106 @@
+
+
+<?php
+$reqmethod = $_SERVER['REQUEST_METHOD'];
+
+
+
+//ensure users can't push data that is not in their own user
+function checkSensors($conn, $user, $sname) {    
+    $sql = "SELECT * FROM sensor S
+INNER JOIN room R ON R.room_id = S.room_id
+INNER JOIN floor F ON F.floor_id = R.floor_id
+INNER JOIN building B ON B.building_id = F.building_id
+INNER JOIN users U ON B.user_id = U.user_id
+WHERE U.user_uname = '$user'
+AND S.sensor_id = '$sname';";
+    $result = mysqli_query($conn,  $sql);
+    $resultCheck = mysqli_num_rows($result);
+    if ($resultCheck > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function pushData($conn, $sname, $date, $time, $metric) {
+    $sql = "INSERT INTO reading(sensor_id, reading_date, reading_time, reading_value) VALUES ($sname, '$date', '$time', $metric);";
+    $result = mysqli_query($conn, $sql);
+    if($result) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if($reqmethod == "GET") {
+
+} else if ($reqmethod == "POST") {
+    if (isset($_REQUEST[sensorname]) && isset($_REQUEST[date]) && isset($_REQUEST[time]) && isset($_REQUEST[metric]) && isset($_REQUEST[username]) && isset($_REQUEST[password])){
+
+        include '../includes/dbh.inc.php';        
+        $uid = mysqli_real_escape_string($conn, $_REQUEST[username]);
+        $pwd = mysqli_real_escape_string($conn, $_REQUEST[password]);
+        $sname = mysqli_real_escape_string($conn, $_REQUEST[sensorname]);
+        $date = mysqli_real_escape_string($conn, $_REQUEST[date]);
+        $time = mysqli_real_escape_string($conn, $_REQUEST[time]);
+        $metric = mysqli_real_escape_string($conn, $_REQUEST[metric]);
+        
+        //Error handlers
+        //Check if inputs are empty
+        if (empty($uid) || empty($pwd)) {
+            echo "error 400";            
+            http_response_code(400);
+        } else {
+            //Does user exist in DB
+            $sql = "SELECT * FROM `users` WHERE user_uname='$uid' ";
+            $result = mysqli_query($conn, $sql);
+            $resultCheck = mysqli_num_rows($result);
+            if ($resultCheck < 1) {
+                http_response_code(400);
+            }  elseif($resultCheck > 1) {
+                echo "error 500";                
+                http_response_code(500);
+            } else {
+                //take data from db and insert it into an array $row
+                //Echo username: echo $row['user_uname'];
+
+                if ($row = mysqli_fetch_assoc($result)) {
+                    //De-hashig the password password_verify(user entered password, db password);
+
+                    $hashedPwd = $row['user_pwd'];
+                    $hashedPwdCheck = password_verify($pwd, $row['user_pwd']);
+                    if ($hashedPwdCheck == false) {
+                        echo "error 401";                        
+                        http_response_code(401);
+                        exit();
+                    } elseif ($hashedPwdCheck == true) {
+                        //check to see if requested sensors to push data is linked to the user doing it
+                        if(checkSensors($conn, $uid, $sname)) {
+                                if(pushData($conn, $sname, $date, $time, $metric)){
+                                    echo "success";
+                                    http_response_code(200);
+                                    exit();
+                                } else {
+                                    http_response_code(400);
+                                    exit();
+                                }
+                        } else {
+                            echo "error 401";
+                            http_response_code(401);
+                        }
+                    }
+                }
+            }
+        }
+
+        } else {
+        http_response_code(400);
+        }
+    exit();
+         
+} else {
+    http_response_code(405);
+}
+
+?>
---
+++ src/build/header.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+  ?>
+
+<!DOCTYPE html>
+<html>
+  <header>
+    <title>Building Management System</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <nav class="navbar navbar-default nav-color">
+      <div class="container-fluid">
+
+	<!-- Brand and toggle get grouped for better mobile display -->
+	<div class="navbar-header">
+	  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+	  </button>
+	  <a class="navbar-brand" href="#"><img class="img-responsive imgfit " src="img/bms.png"/></a>
+	</div>
+
+	<!-- Collect the nav links, forms, and other content for toggling -->
+	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	  <ul class="nav navbar-nav">
+	    <li><a  href='index.php'>Home</a></li>	    
+	    <?php
+    if (!isset($_SESSION['u_id'])) {
+    } else {
+              echo "<li><a href='monitor.php'>Monitor</a></li>";        
+    }
+	      ?>
+			
+	  </ul>
+	  <ul class="nav navbar-nav navbar-right">
+	    <?php
+    if (isset($_SESSION['u_id'])) {
+        echo "<li><a href='settings.php'>", $_SESSION['u_uname'], "</a></li>";
+
+        echo "<li>
+	      <form class='btn btn-pad' action='includes/logout.inc.php' name='submit' method='POST'>
+		<button type='submit' name='submit'  class='btn-link'>Logout</button>
+	      </form>
+	    </li>";
+    } else {
+         echo "<li><a href='login.php'>Login</a></li>";
+         echo "<li><a href='register.php'>Register</a></li>";         
+    }
+?>
+	  </ul>
+	</div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+  </header>
+  <body>
